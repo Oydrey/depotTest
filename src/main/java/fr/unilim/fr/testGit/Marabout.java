033d6b4ee32fe65e3efcda53a1643f050c28e147
iut <iut@10.0.2.15>
2019-04-01 14:59:26 +0200

    suite jeu marabout 3 phrases de plus (c ouf)

--- src/main/java/fr/unilim/fr/testGit/Marabout.java
+++ src/main/java/fr/unilim/fr/testGit/Marabout.java
@@ -6,6 +6,9 @@ public class Marabout {
 
 	    return "Trois p'tits chats, trois p'tits chats, trois p'tits chats, chats, chats, \n"
 			   + "Chapeau d'paille, chapeau d'paille, chapeau d'paille, paille, paille, \n"
-			   + "Paillasson, paillasson, paillasson, -son, -son,";
+			   + "Paillasson, paillasson, paillasson, -son, -son,"
+				+ "Somnambule, somnambule, somnambule, -bule, -bule, \n"
+				+ "Bulletin, bulletin, bulletin, -tin, -tin, \n"
+				+ "Tintamarre, tintamarre, tintamarre, -marre, -marre,";
      }
 }
